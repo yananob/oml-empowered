bdb8a34522c9bc7b133343428a5f4bc14144a451
yananob <yananob@gmail.com>
2024-12-13 20:37:29 +0900

    init

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eu
+
+# choose deploy type
+# bash ./_cf-common/deploy/deploy_php_http.sh . {CLOUD_FUNCTION_NAME}
+# bash ./_cf-common/deploy/deploy_php_topic.sh . {CLOUD_FUNCTION_NAME}
